5c874202798a82a5aa741cdd3ffba29bb66bfc0b
mmahdibarghi <mahdi_b7978@yahoo.com>
2022-12-17 17:56:21 +0330

    some update

--- Mehrdad-Function.sql
+++ Mehrdad-Function.sql
@@ -8,5 +8,7 @@ RETURN
   select avg(PayToCharity.Amount) as AvaregePayment
   from Helper inner join PayToCharity on Helper.HelperID = PayToCharity.HelperID
   WHERE DATEDIFF(day, GETDATE(), PayToCharity.PaymentDate) <= 365
-  and Helper.NationalCode = '2048854941' 
+  and Helper.NationalCode = @HelperNationalCode
 );
+
+
